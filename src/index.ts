f132da53442c44820b9f4f7e74f86d243995017f
spen428 <77985620+spen428@users.noreply.github.com>
2025-01-05 11:45:30 +0900

    Remove PasteInput listener

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import {captureHoldFullRs, PasteInput} from "@alt1/base";
+import {captureHoldFullRs} from "@alt1/base";
 import "./index.html";
 import "./appconfig.json";
 import "./icon.png";
-import Logger from "./util/Logger";
 import StatueCollectionBagImageProcessor from "./util/StatueCollectionBagImageProcessor";
 import {CAPTURE_INTERVAL_MS} from "./config";
 
-function captureAndProcessChatBox() {
+function captureAndProcessGameWindow() {
     if (!window.alt1) {
         return;
     }
@@ -18,7 +17,7 @@ function captureAndProcessChatBox() {
 function start() {
     if (window.alt1) {
         alt1.identifyAppUrl("./appconfig.json");
-        setInterval(() => captureAndProcessChatBox(), CAPTURE_INTERVAL_MS);
+        setInterval(() => captureAndProcessGameWindow(), CAPTURE_INTERVAL_MS);
     } else {
         const addAppUrl = `alt1://addapp/${new URL("./appconfig.json", document.location.href).href}`;
         document
@@ -28,13 +27,6 @@ function start() {
                 `Alt1 not detected, click <a href='${addAppUrl}'>here</a> to add this app to Alt1.`,
             );
     }
-
-    PasteInput.listen(
-        (img) => {
-            StatueCollectionBagImageProcessor.processScreenshot(img);
-        },
-        (err, errid) => Logger.writeError(`${errid} ${err}`),
-    );
 }
 
 start();
