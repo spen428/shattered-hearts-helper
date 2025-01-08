1170534716296a757f70af7e56ff22a3d88dd395
spen428 <77985620+spen428@users.noreply.github.com>
2025-01-08 18:52:04 +0900

    Add chatbox test screenshots

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,15 +1,21 @@
 import {ImageDetect, ImgRefData} from "@alt1/base";
-import StatueCollectionBagImageProcessor, {bagState, RockBag} from "../util/StatueCollectionBagImageProcessor";
+import StatueCollectionBagImageProcessor from "../util/StatueCollectionBagImageProcessor";
+import Logger from "../util/Logger";
+import {bagState, RockBag} from "../util/StatueCollectionBag";
 
 const testImages = ImageDetect.webpackImages({
     strangeRocksEmpty: require("../tests/res/strange-empty.data.png"),
     strangeRocksPartial: require("../tests/res/strange-farming-smithing.data.png"),
     goldenRocksEmpty: require("../tests/res/golden-empty.data.png"),
     goldenRocksPartial: require("../tests/res/golden-crafting-2melee-2prayer.data.png"),
+    chatRockGained: require("./res/agig.data.png"),
+    chatRockGained2: require("../tests/res/agis.data.png"),
+    chatRockGained3: require("../tests/res/agis2.data.png"),
 });
 
 function assert(bool: boolean, msg: string) {
     if (!bool) {
+        Logger.writeError("Assertion failed: " + msg);
         throw new Error("Assertion failed: " + msg);
     }
 }
@@ -51,6 +57,26 @@ function runTests() {
     StatueCollectionBagImageProcessor.processScreenshot(new ImgRefData(testImages.strangeRocksEmpty));
     assertEmpty(bagState.strangeRocks);
     assertEmpty(bagState.goldenRocks);
+
+    StatueCollectionBagImageProcessor.processScreenshot(new ImgRefData(testImages.chatRockGained));
+    assertEmpty(bagState.strangeRocks);
+    assertRockCount(bagState.goldenRocks, 2);
+    assert(bagState.goldenRocks.agility === 2, "Expected 2 golden agility rocks");
+
+    StatueCollectionBagImageProcessor.processScreenshot(new ImgRefData(testImages.chatRockGained2));
+    assertRockCount(bagState.strangeRocks, 1);
+    assert(bagState.goldenRocks.agility === 1, "Expected 1 agility rock");
+    assertRockCount(bagState.goldenRocks, 2);
+    assert(bagState.goldenRocks.agility === 2, "Expected 2 golden agility rocks");
+
+    StatueCollectionBagImageProcessor.processScreenshot(new ImgRefData(testImages.chatRockGained3));
+    assertRockCount(bagState.strangeRocks, 2);
+    assert(bagState.strangeRocks.agility === 2, "Expected 2 agility rocks");
+    assertRockCount(bagState.goldenRocks, 2);
+    assert(bagState.goldenRocks.agility === 2, "Expected 2 golden agility rocks");
 }
 
-testImages.promise.then(() => runTests());
+testImages.promise.then(() => {
+    runTests();
+    Logger.writeLog("All tests passed!");
+});
