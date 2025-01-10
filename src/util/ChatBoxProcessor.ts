351ce7b22426ad2dc15bb078b41284c3356891c3
spen428 <77985620+spen428@users.noreply.github.com>
2025-01-10 11:55:42 +0900

    Null checks

--- src/util/ChatBoxProcessor.ts
+++ src/util/ChatBoxProcessor.ts
@@ -6,13 +6,18 @@ class ChatBoxProcessor {
   private readonly chatBoxReader = new ChatBoxReader();
 
   getChatLines(img: ImgRef): ChatLine[] {
+    if (!img) {
+      Logger.writeWarning("ImgRef is null");
+      return;
+    }
+
     if (!this.chatBoxReader.pos) {
       this.chatBoxReader.find(img);
     }
 
     const chatLines = this.chatBoxReader.read(img);
     if (!chatLines?.length) {
-      Logger.writeError("No chat lines");
+      Logger.writeWarning("No chat lines");
       return [];
     }
 
