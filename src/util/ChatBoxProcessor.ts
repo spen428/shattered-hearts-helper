75da4ceb36a16a7d1d4922439a9c6c42e418949e
spen428 <77985620+spen428@users.noreply.github.com>
2025-01-10 10:43:33 +0900

    Check for chatReader.pos before calling find

--- src/util/ChatBoxProcessor.ts
+++ src/util/ChatBoxProcessor.ts
@@ -1,19 +1,23 @@
-import ChatBoxReader, {ChatLine} from "@alt1/chatbox";
-import {ImgRef} from "@alt1/base";
+import ChatBoxReader, { ChatLine } from "@alt1/chatbox";
+import { ImgRef } from "@alt1/base";
 import Logger from "./Logger";
 
 class ChatBoxProcessor {
-    private readonly chatBoxReader = new ChatBoxReader();
+  private readonly chatBoxReader = new ChatBoxReader();
 
-    getChatLines(img: ImgRef): ChatLine[] {
-        const chatLines = this.chatBoxReader.read(img);
-        if (!chatLines?.length) {
-            Logger.writeError("No chat lines");
-            return [];
-        }
-
-        return chatLines;
+  getChatLines(img: ImgRef): ChatLine[] {
+    if (!this.chatBoxReader.pos) {
+      this.chatBoxReader.find(img);
     }
+
+    const chatLines = this.chatBoxReader.read(img);
+    if (!chatLines?.length) {
+      Logger.writeError("No chat lines");
+      return [];
+    }
+
+    return chatLines;
+  }
 }
 
 export default new ChatBoxProcessor();
