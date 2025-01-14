84e5b3a5ca28399f8237f0bc0983bc1f65eaa2d7
spen428 <77985620+spen428@users.noreply.github.com>
2025-01-14 16:54:44 +0900

    Simplify UiRenderer by adding StatueCollectionBag.enumerate() method

--- src/util/StatueCollectionBag.ts
+++ src/util/StatueCollectionBag.ts
@@ -1,39 +1,81 @@
-export type RockBag = { [key: string]: { first: boolean, second: boolean } };
+export type RockBag = { [key: string]: { first: boolean; second: boolean } };
 
 export const bagState = {
-    strangeRocks: {
-        Agility: { first: false, second: false },
-        Construction: { first: false, second: false },
-        Cooking: { first: false, second: false },
-        Crafting: { first: false, second: false },
-        Farming: { first: false, second: false },
-        Firemaking: { first: false, second: false },
-        Fishing: { first: false, second: false },
-        Fletching: { first: false, second: false },
-        Herblore: { first: false, second: false },
-        Hunter: { first: false, second: false },
-        Mining: { first: false, second: false },
-        Runecrafting: { first: false, second: false },
-        Smithing: { first: false, second: false },
-        Thieving: { first: false, second: false },
-        Woodcutting: { first: false, second: false },
-    },
-    goldenRocks: {
-        Agility: { first: false, second: false },
-        Construction: { first: false, second: false },
-        Crafting: { first: false, second: false },
-        Divination: { first: false, second: false },
-        Dungeoneering: { first: false, second: false },
-        Farming: { first: false, second: false },
-        Herblore: { first: false, second: false },
-        Magic: { first: false, second: false },
-        Melee: { first: false, second: false },
-        Mining: { first: false, second: false },
-        Prayer: { first: false, second: false },
-        Ranged: { first: false, second: false },
-        Slayer: { first: false, second: false },
-        Smithing: { first: false, second: false },
-        Summoning: { first: false, second: false },
-        Woodcutting: { first: false, second: false },
-    },
+  strangeRocks: {
+    Agility: { first: false, second: false },
+    Construction: { first: false, second: false },
+    Cooking: { first: false, second: false },
+    Crafting: { first: false, second: false },
+    Farming: { first: false, second: false },
+    Firemaking: { first: false, second: false },
+    Fishing: { first: false, second: false },
+    Fletching: { first: false, second: false },
+    Herblore: { first: false, second: false },
+    Hunter: { first: false, second: false },
+    Mining: { first: false, second: false },
+    Runecrafting: { first: false, second: false },
+    Smithing: { first: false, second: false },
+    Thieving: { first: false, second: false },
+    Woodcutting: { first: false, second: false },
+  },
+  goldenRocks: {
+    Agility: { first: false, second: false },
+    Construction: { first: false, second: false },
+    Crafting: { first: false, second: false },
+    Divination: { first: false, second: false },
+    Dungeoneering: { first: false, second: false },
+    Farming: { first: false, second: false },
+    Herblore: { first: false, second: false },
+    Magic: { first: false, second: false },
+    Melee: { first: false, second: false },
+    Mining: { first: false, second: false },
+    Prayer: { first: false, second: false },
+    Ranged: { first: false, second: false },
+    Slayer: { first: false, second: false },
+    Smithing: { first: false, second: false },
+    Summoning: { first: false, second: false },
+    Woodcutting: { first: false, second: false },
+  },
 };
+
+class StatueCollectionBag {
+  enumerate(): Rock[] {
+    const rocks: Rock[] = [];
+    const bags = {
+      strange: bagState.strangeRocks,
+      golden: bagState.goldenRocks,
+    };
+
+    for (const rockType of rockTypes) {
+      const rockBag = bags[rockType];
+      const skills = Object.keys(rockBag);
+
+      for (const skill of skills) {
+        for (const ordinal of rockOrdinals) {
+          rocks.push({
+            type: rockType,
+            skill,
+            ordinal,
+            isPresent: rockBag[skill][ordinal],
+          });
+        }
+      }
+    }
+    return rocks;
+  }
+}
+
+const rockTypes = ["strange", "golden"] as const;
+type RockType = typeof rockTypes[number];
+
+const rockOrdinals = ["first", "second"] as const;
+type RockOrdinal = typeof rockOrdinals[number];
+
+export interface Rock {
+  type: RockType;
+  skill: string;
+  ordinal: RockOrdinal;
+  isPresent: boolean;
+}
+
+export default new StatueCollectionBag();
--- src/util/UiRenderer.ts
+++ src/util/UiRenderer.ts
@@ -1,22 +1,12 @@
-import { bagState } from "./StatueCollectionBag";
+import StatueCollectionBag from "./StatueCollectionBag";
 
 class UiRenderer {
   rerender(document: Document) {
-    const bags = [
-      { bag: bagState.strangeRocks, key: "Strange" },
-      { bag: bagState.goldenRocks, key: "Golden" },
-    ];
-    for (const bag of bags) {
-      for (const skill in bag.bag) {
-        const rocks = bag.bag[skill];
-        for (const rockOrdinal in rocks) {
-          const isRockPresent = rocks[rockOrdinal];
-          const imgElement = document.querySelector(
-            `#${bag.key}-${skill}-${rockOrdinal}`,
-          ) as HTMLImageElement;
-          imgElement.className = isRockPresent ? "rock-in-bag" : "rock-missing";
-        }
-      }
+    for (const rock of StatueCollectionBag.enumerate()) {
+      const imgElement = document.querySelector(
+        `#${rock.type}-${rock.skill}-${rock.ordinal}`,
+      ) as HTMLImageElement;
+      imgElement.className = rock.isPresent ? "rock-in-bag" : "rock-missing";
     }
   }
 }
