c9a8401942a88d2be7bef81d03f7a17d5c8c02a6
spen428 <77985620+spen428@users.noreply.github.com>
2025-01-20 11:56:16 +0900

    Replace images for detecting which statue collection bag tab is open.

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,6 @@ export const CAPTURE_INTERVAL_MS = 2000;
 
 export const subImages = ImageDetect.webpackImages({
   statueCollectionBagHeader: require("./res/statue-collection-bag.data.png"),
-  whitePixel: require("./res/white-pixel.data.png"),
   strangeRocksTabSelected: require("./res/strange-tab.data.png"),
   goldenRocksTabSelected: require("./res/golden-tab.data.png"),
 });
